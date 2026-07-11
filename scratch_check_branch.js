ddeba24de698a8ed958d57699cb2ed2d9e82b4b7
Saiko <maksum2007123@gmail.com>
2026-07-11 18:39:11 +0300

    chore: remove test scripts

--- scratch_check_branch.js
+++
@@ -1,7 +0,0 @@
-
-async function check() {
-  const res = await fetch(`https://api.github.com/repos/ddidif/submarinemilkkk/commits?sha=vanilla%2B&per_page=1`);
-  const json = await res.json();
-  console.log(JSON.stringify(json[0].commit, null, 2));
-}
-check();
